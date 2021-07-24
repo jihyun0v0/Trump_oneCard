54d876c0301aee3093a98d46c2b43fa9c6f7587e
memoryBangwool <jihyuneee01@gmail.com>
2021-07-24 10:34:48 +0900

    add to develop

---
+++ oneCard/oneCard/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  oneCard
+//
+//  Created by Jihyun Kyoung on 2021/07/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
