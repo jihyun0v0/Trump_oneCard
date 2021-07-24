ddd402cd2b698a987b34a69868acb245abeb8707
memoryBangwool <jihyuneee01@gmail.com>
2021-07-24 10:59:43 +0900

    add attack

--- memoryBangwool/One_card/card.c
+++ memoryBangwool/One_card/card.c
@@ -1,9 +1,53 @@
-void attack()
+int attack(int index)
 {
-    // A, 2, Joker
+    while((Me.hand[index++])!=-1);
+    index--;
+    
+    if(!check_attack(index)){
+        printf("your card is not right\n");
+        return -1;
+    }
+    
+    int Card=Me.hand[index];
+    
+    switch (Card) {
+        case 52:
+            return 9;
+            break;
+        case 53:
+            return 7;
+            break;
+        default:
+            switch(Card%13){
+                case 0:
+                if((Card/13)==SPDE)
+                    return 5;
+                return 3;
+                case 1:
+                    return 2;
+                default:
+                    return 0;
+            }
+    }
+    
+    // A=0(3), 2=1(2), Joker=52,53
+    
 }
 
 void defend()
 {
     // A, 2, 3, Joker
+}
+
+
+int check_attack(int index){
+    int card=Me.hand[index];
+    int test_card=stack[ind];
+    
+    if(card>=52)
+        return 1;
+    if(card%13==test_card%13||card/13==test_card/13)
+        return 1;
+    return 0;
+    
 }
