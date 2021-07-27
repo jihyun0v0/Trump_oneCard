2292802f0a36ffc3a9852e2de781129fbfe26299
memoryBangwool <jihyuneee01@gmail.com>
2021-07-27 10:31:18 +0900

    change main.c

--- memoryBangwool/main.c
+++ memoryBangwool/main.c
@@ -36,6 +36,8 @@ void print_shpe(int);
 void print_num(int);
 void put_card(int num);
 int set_attack(int *index);
+int attack(int index);
+void defend(void);
 
 
 int main(){
@@ -167,3 +169,46 @@ int set_attack(int * index){
         return 0;
     
 }
+
+int attack(int index)
+{
+    while((Me.hand[index++])!=-1);
+    index--;
+    
+    if(!set_attack(index)){
+        printf("your card is not right\n");
+        return -1;
+    }
+    
+    int Card=Me.hand[index];
+    
+    switch (Card) {
+        case 52:
+            return 9;
+            break;
+        case 53:
+            return 7;
+            break;
+        default:
+            switch(Card%13){
+                case 0:
+                if((Card/13)==SPDE)
+                    return 5;
+                return 3;
+                case 1:
+                    return 2;
+                default:
+                    return 0;
+            }
+    }
+    
+    // A=0(3), 2=1(2), Joker=52,53
+    
+}
+
+void defend()
+{
+    // A, 2, 3, Joker
+}
+
+
